32abd5a3f576d8a97e386a80ab83b3d03ca3c708
Kanghj <rkdguswns0803@gmail.com>
2022-09-30 11:46:19 +0900

    login 추가

--- js/clock.js
+++ js/clock.js
@@ -5,12 +5,15 @@ const clock = document.querySelector("#clock");
 function getClock() {
   // 현재 시간을 가져오는 객체
   const date = new Date();
+  // 00:00:00 format을 맞추기 위해 String의 padStart 사용
+  // 숫자형을 문자형으로 바꿀때 숫자를 연산할 일이 있는지 생각하고 바꾸자
   const hours = String(date.getHours()).padStart(2, "0");
   const minutes = String(date.getMinutes()).padStart(2, "0");
   const seconds = String(date.getSeconds()).padStart(2, "0");
 
   // clock 객체에 시간 추가
   clock.innerHTML = `${hours}:${minutes}:${seconds}`;
+  // return을 통해 현재 시간을 반환하는 방법
   // return `${hours}:${minutes}:${seconds}`;
 }
 // 함수를 통해 시간 문자열을 할당
