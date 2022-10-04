5f9f9620b62d3e1e97b5a95dd0e86c21b5a12777
Kanghj <rkdguswns0803@gmail.com>
2022-10-04 14:15:04 +0900

    JQuery log in

--- js/login.js
+++ js/login.js
@@ -1,20 +1,20 @@
 // 입력 form에 작성한 이름 가져오기 - form의 submit 이벤트 사용
-const loginForm = document.querySelector("#login-form");
+const loginForm = $("#login-form");
 
 // name 값을 가져오기 위한 input
-const loginInput = document.querySelector("#login-form #name");
+const loginInput = $("#login-form #name");
 // name을 가질 h1
-const title = document.querySelector("#title");
+const title = $("#title");
 
 // event listener 연결
 // form의 submit을 사용할 경우 click event가 아니라
 // form의 submit event로 연결
-loginForm.addEventListener("submit", onLoginSubmit);
+loginForm.on("submit", onLoginSubmit);
 
 function onLoginSubmit(e) {
   e.preventDefault();
   // loginInput의 value값을 가져옴
-  const name = loginInput.value;
+  const name = loginInput.val();
 
   // if문을 사용, name값이 없을 때 아래의 내용이 실행되지 않음
   // 함수에서 return을 사용하면 함수 종료
@@ -23,9 +23,9 @@ function onLoginSubmit(e) {
   }
   // value값을 title에 넣어줌
   console.log(name);
-  title.innerHTML = name + "님 반갑습니다.";
+  title.html(name + "님 반갑습니다.");
   // loginForm hidden class 추가
-  loginForm.classList.add("hidden");
+  loginForm.addClass("hidden");
   // todo-form을 가져와 hidden class 제거
-  document.querySelector("#todo-form").classList.remove("hidden");
+  $("#todo-form").removeClass("hidden");
 }
