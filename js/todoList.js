e19768c73a9bb76b57ea2ba4b2d009d17b23fbc0
Kanghj <rkdguswns0803@gmail.com>
2022-10-04 15:12:01 +0900

    JQuery add list

--- js/todoList.js
+++ js/todoList.js
@@ -1,47 +1,45 @@
 // todo 입력공간 가져오기
-const todoInput = document.querySelector("#todo-form #todo");
+const todoInput = $("#todo-form #todo");
 
 // button -> click event로 실행
 // button에 id를 주지 않았기 때문에, 노드로 접근하거나
 // querySelector를 이용해 선택자로 접근하여 객체를 가져온다
-const todoButton = todoInput.nextElementSibling;
+const todoButton = todoInput.next();
 
 // todo board 객체 가져오기
-const todoBoard = document.querySelector("#todo-board");
+const todoBoard = $("#todo-board");
 
 // button의 event listener : click event
-todoButton.addEventListener("click", addTodo);
+todoButton.on("click", addTodo);
 
 function addTodo() {
   console.log("확인");
   // todoInput - value 값을 들고옴
-  const text = todoInput.value;
+  const text = todoInput.val();
   // 새로운 요소 생성
   // li, input type="checkbox", textNode, button
-  const li = document.createElement("li");
-  const checkbox = document.createElement("input");
-  checkbox.type = "checkbox";
+  const li = $("<li>");
+  const checkbox = $("<input>").attr("type", "checkbox");
 
-  const textNode = document.createTextNode(text);
-  const button = document.createElement("button");
+  const textNode = $("<span>").html(text);
+  const button = $("<button>");
 
   // li 요소 - input, textNode, button 추가
-  li.appendChild(checkbox);
-  li.appendChild(textNode);
-  li.appendChild(button);
+  li.append(checkbox).append(textNode).append(button);
+
   // todoBoard - li 추가
   todoBoard.append(li);
 
   // todoInput.value값을 빈값으로
-  todoInput.value = "";
+  todoInput.val("");
   todoInput.focus();
   // button에 X 추가
-  button.innerHTML = "X";
+  button.html("X");
   // checkbox를 눌렀을 때 event listener 실행 : click
-  checkbox.addEventListener("click", todoCheck);
+  checkbox.on("click", todoCheck);
 
   // x button을 눌렀을 때 event listener 실행 : click
-  button.addEventListener("click", todoDelete);
+  button.on("click", todoDelete);
 }
 
 // checkbox event listener에 들어갈 함수
